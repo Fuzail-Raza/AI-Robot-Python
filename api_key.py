75f96c533e0e2d56d518ec2d25bb9c902b04d5f1
Fuzail Raza <fuzailraza161@gmail.com>
2023-08-28 01:56:25 +0500

    Update MongoDB URL

--- api_key.py
+++ api_key.py
@@ -3,3 +3,5 @@ apikey=<OpenAI-API-KEY>
 # link=https://platform.openai.com/account/api-keys
 weather_api=<weather_apiKEY>
 # link=https://www.weatherapi.com/
+MongoDBURL=<MongoDB Atlas>
+# https://www.mongodb.com/products/platform/cloud
